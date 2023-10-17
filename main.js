cce8644bc565d3df489c0a9a8ff3ddd95e4ffabd
MuzGeek <emamuzo.okerri@gmail.com>
2023-10-17 15:17:47 +0100

    introduced stters to watch validation status and enable button

--- main.js
+++ main.js
@@ -3,18 +3,53 @@
 // Base URL for API
 const base = "http://localhost:3000";
 
-// Create account input fields
+// Create account form fields and button
 const inputFname = document.querySelector("#fname");
 const inputLname = document.querySelector("#lname");
 const inputEmail = document.querySelector("#email");
 const inputPass = document.querySelector("#password");
 const inputPassConfirm = document.querySelector("#pass-confirm");
-
-// Create account button
 const btnRegister = document.querySelector(".btn__register");
 
-// Array of all create account input fields
-const inputsRegister = [inputFname, inputLname, inputEmail, inputPass, inputPassConfirm];
+const data = {
+	formData: {
+		firstname: "",
+		lastname: "",
+		email: "",
+		password: "",
+	},
+	validationStatus: [false, false, false, false],
+
+	enableBtn() {
+		if (this.validationStatus.every((el) => el === true))
+			btnRegister.removeAttribute("disabled", "");
+		else btnRegister.setAttribute("disabled", "");
+	},
+
+	set fnameStatus(val) {
+		this.validationStatus[0] = val;
+
+		this.enableBtn();
+	},
+
+	set lnameStatus(val) {
+		this.validationStatus[1] = val;
+
+		this.enableBtn();
+	},
+
+	set emailStatus(val) {
+		this.validationStatus[2] = val;
+
+		this.enableBtn();
+	},
+
+	set passStatus(val) {
+		this.validationStatus[3] = val;
+
+		this.enableBtn();
+	},
+};
 
 // Array of possible error messages
 const errMssgs = [
@@ -25,12 +60,36 @@ const errMssgs = [
 
 let err;
 let strgthMeter;
-const validStatus = [false, false, false, false, false];
+// const validStatus = [false, false, false, false, false];
 
 // Generate hint for input fields
 const generateHint = function (mssg) {
 	return `<p class="input__error">${mssg}</p>`;
 };
+
+inputFname.addEventListener("blur", function (e) {
+	const input = e.target;
+
+	if (!input.value && !input.nextElementSibling) {
+		input.insertAdjacentHTML("afterend", generateHint(errMssgs[0]));
+		data.fnameStatus = false;
+	} else {
+		if (input.nextElementSibling) input.nextElementSibling.remove();
+		data.fnameStatus = true;
+	}
+});
+
+inputLname.addEventListener("blur", function (e) {
+	const input = e.target;
+
+	if (!input.value && !input.nextElementSibling) {
+		input.insertAdjacentHTML("afterend", generateHint(errMssgs[0]));
+		data.lnameStatus = false;
+	} else {
+		if (input.nextElementSibling) input.nextElementSibling.remove();
+		data.lnameStatus = true;
+	}
+});
 
 // Validate email entry
 inputEmail.addEventListener("blur", function (e) {
@@ -41,18 +100,15 @@ inputEmail.addEventListener("blur", function (e) {
 			/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 		if (!emailRegex.test(input.value)) {
+			data.emailStatus = false;
 			if (!input.nextElementSibling) {
 				input.insertAdjacentHTML("afterend", generateHint(errMssgs[1]));
-				validStatus[2] = false;
 			} else {
 				input.nextElementSibling.textContent = errMssgs[1];
-				validStatus[2] = false;
 			}
 		} else {
-			if (input.nextElementSibling) {
-				input.nextElementSibling.remove();
-				validStatus[2] = true;
-			}
+			data.emailStatus = true;
+			if (input.nextElementSibling) input.nextElementSibling.remove();
 		}
 	}
 });
@@ -145,11 +201,11 @@ inputPass.addEventListener("blur", function (e) {
 		input.nextElementSibling.remove();
 	}
 
-	if (input.value) {
-		validStatus[3] = true;
+	/* if (input.value) {
+		data.passStatus = true;
 	} else {
-		validStatus[3] = false;
-	}
+		data.passStatus = false;
+	} */
 });
 
 // Confirm password input: Blur event
@@ -159,7 +215,7 @@ inputPassConfirm.addEventListener("blur", function (e) {
 	// Check for password match
 	if (inputPass.value && input.value) {
 		if (input.value !== inputPass.value) {
-			validStatus[4] = false;
+			data.passStatus = false;
 
 			if (!input.nextElementSibling) {
 				input.insertAdjacentHTML("afterend", generateHint(errMssgs[2]));
@@ -167,11 +223,11 @@ inputPassConfirm.addEventListener("blur", function (e) {
 				input.nextElementSibling.textContent = errMssgs[2];
 			}
 		} else {
-			validStatus[4] = true;
+			data.passStatus = true;
 
 			if (input.nextElementSibling) input.nextElementSibling.remove();
 		}
-	} else validStatus[4] = false;
+	} else data.passStatus = false;
 });
 
 /* 
